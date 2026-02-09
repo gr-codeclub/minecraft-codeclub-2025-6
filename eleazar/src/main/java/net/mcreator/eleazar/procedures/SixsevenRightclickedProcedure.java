863dd350a4d100c0bf22b15458690818cd4667c2
codeclub20236 <134069199+codeclub20236@users.noreply.github.com>
2026-02-09 17:05:21 +0000

    HEY

--- eleazar/src/main/java/net/mcreator/eleazar/procedures/SixsevenRightclickedProcedure.java
+++ eleazar/src/main/java/net/mcreator/eleazar/procedures/SixsevenRightclickedProcedure.java
@@ -7,14 +7,26 @@ import net.mcreator.eleazar.init.EleazarModBlocks;
 
 public class SixsevenRightclickedProcedure {
 	public static void execute(LevelAccessor world, double y, double z) {
+		boolean deletedthisrow = false;
 		double xoffset = 0;
 		double zoffset = 0;
+		double minedcount = 0;
+		double xMax = 0;
+		double xPos = 0;
+		double max = 0;
 		xoffset = -1;
 		zoffset = -1;
 		for (int index0 = 0; index0 < 3; index0++) {
 			for (int index1 = 0; index1 < 3; index1++) {
-				world.setBlock(BlockPos.containing(xoffset, y - 2, z), EleazarModBlocks.KPOPSUCKS.get().defaultBlockState(), 3);
+				world.setBlock(BlockPos.containing(y - 2, 0, z), EleazarModBlocks.KPOPSUCKS.get().defaultBlockState(), 3);
+				xoffset = xoffset + 1;
 			}
+			xoffset = -1;
+			xoffset = xoffset + 1;
+		}
+		while (deletedthisrow) {
+			minedcount = minedcount + 1;
+			xMax = xPos + max + xMax;
 		}
 	}
 }
