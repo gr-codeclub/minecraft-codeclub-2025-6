6a3552823e4f5a259c03b68ba0102e6bdf954cc2
codeclub202319 <134315418+codeclub202319@users.noreply.github.com>
2026-02-23 16:43:08 +0000

    DiceBlock many diffrent eventsb happen 6 sides

---
+++ iko/ilovesoccer/src/main/java/net/mcreator/ilovesoccer/procedures/DiceBlockOnBlockRightclickedProcedure.java
@@ -0,0 +1,64 @@
+package net.mcreator.ilovesoccer.procedures;
+
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.LightningBolt;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.EntitySpawnReason;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.damagesource.DamageTypes;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.util.RandomSource;
+import net.minecraft.util.Mth;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.network.chat.Component;
+import net.minecraft.core.BlockPos;
+import net.minecraft.advancements.AdvancementProgress;
+import net.minecraft.advancements.AdvancementHolder;
+
+public class DiceBlockOnBlockRightclickedProcedure {
+	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
+		if (entity == null)
+			return;
+		double roll = 0;
+		roll = Mth.nextInt(RandomSource.create(), 1, 6);
+		if (entity instanceof Player _player && !_player.level().isClientSide())
+			_player.displayClientMessage(Component.literal(("You rolled " + new java.text.DecimalFormat("##").format(roll) + "!")), false);
+		if (roll == 1) {
+			if (world instanceof ServerLevel _level) {
+				LightningBolt entityToSpawn = EntityType.LIGHTNING_BOLT.create(_level, EntitySpawnReason.TRIGGERED);
+				entityToSpawn.snapTo(Vec3.atBottomCenterOf(BlockPos.containing(entity.getX(), entity.getY(), entity.getZ())));;
+				_level.addFreshEntity(entityToSpawn);
+			}
+		} else if (roll == 2) {
+			if (world instanceof ServerLevel _level) {
+				Entity entityToSpawn = EntityType.GHAST.spawn(_level, BlockPos.containing(x + 2, y, z), EntitySpawnReason.MOB_SUMMONED);
+				if (entityToSpawn != null) {
+					entityToSpawn.setYRot(world.getRandom().nextFloat() * 360F);
+				}
+			}
+		} else if (roll == 4) {
+			entity.hurt(new DamageSource(world.holderOrThrow(DamageTypes.DRAGON_BREATH)), entity instanceof LivingEntity _livEnt ? _livEnt.getHealth() : -1);
+		} else if (roll == 5) {
+			if (entity instanceof ServerPlayer _player && _player.level() instanceof ServerLevel _level) {
+				AdvancementHolder _adv = _level.getServer().getAdvancements().get(ResourceLocation.parse("minecraft:end/kill_dragon"));
+				if (_adv != null) {
+					AdvancementProgress _ap = _player.getAdvancements().getOrStartProgress(_adv);
+					if (!_ap.isDone()) {
+						for (String criteria : _ap.getRemainingCriteria())
+							_player.getAdvancements().award(_adv, criteria);
+					}
+				}
+			}
+		} else {
+			for (int index0 = 0; index0 < 10; index0++) {
+				if (entity instanceof Player _player)
+					_player.giveExperienceLevels(5);
+			}
+		}
+	}
+}
