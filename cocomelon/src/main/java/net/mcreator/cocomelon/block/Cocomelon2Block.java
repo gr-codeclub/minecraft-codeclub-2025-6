9f4a59bde66259f72efa4454cb774f08d01fad0d
codeclub202319 <134315418+codeclub202319@users.noreply.github.com>
2026-04-16 10:23:48 +0100

    idea and mcreator update

--- cocomelon/src/main/java/net/mcreator/cocomelon/block/Cocomelon2Block.java
+++ cocomelon/src/main/java/net/mcreator/cocomelon/block/Cocomelon2Block.java
@@ -1,6 +1,5 @@
 package net.mcreator.cocomelon.block;
 
-import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.Block;
@@ -8,10 +7,5 @@ import net.minecraft.world.level.block.Block;
 public class Cocomelon2Block extends Block {
 	public Cocomelon2Block(BlockBehaviour.Properties properties) {
 		super(properties.sound(SoundType.GRAVEL).strength(1f, 10f).friction(4f).speedFactor(9f).jumpFactor(39f));
-	}
-
-	@Override
-	public int getLightBlock(BlockState state) {
-		return 15;
 	}
 }
--- cocomelon/src/main/java/net/mcreator/cocomelon/block/HicocomelonBlock.java
+++ cocomelon/src/main/java/net/mcreator/cocomelon/block/HicocomelonBlock.java
@@ -1,6 +1,5 @@
 package net.mcreator.cocomelon.block;
 
-import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.Block;
@@ -8,10 +7,5 @@ import net.minecraft.world.level.block.Block;
 public class HicocomelonBlock extends Block {
 	public HicocomelonBlock(BlockBehaviour.Properties properties) {
 		super(properties.sound(SoundType.GRAVEL).strength(1f, 10f));
-	}
-
-	@Override
-	public int getLightBlock(BlockState state) {
-		return 15;
 	}
 }
