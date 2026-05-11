4cf647118fb2a490ec297502e1701e595e795b91
codeclub202320 <codeclub202320@outlook.com>
2026-05-11 16:45:40 +0100

    refactor

---
+++ iko/ilovesoccer/src/main/java/net/mcreator/ilovesoccer/procedures/CastProcedure.java
@@ -0,0 +1,44 @@
+package net.mcreator.ilovesoccer.procedures;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.scores.Scoreboard;
+import net.minecraft.world.scores.ScoreHolder;
+import net.minecraft.world.scores.Objective;
+import net.minecraft.world.entity.Entity;
+
+public class CastProcedure {
+	public static void execute(Entity entity, double x_coord, double y_coord, double z_coord) {
+		if (entity == null)
+			return;
+		double spell = 0;
+		double x = 0;
+		double y = 0;
+		double z = 0;
+		spell = getEntityScore("custom_score", entity);
+		x = x_coord;
+		y = y_coord;
+		z = z_coord;
+
+		if (spell == 0) {
+			if (entity instanceof Player _player && !_player.level().isClientSide())
+				_player.displayClientMessage(Component.literal("Hi from MCreator"), false);
+		} else if (spell == 1) {
+			if (entity instanceof Player _player && !_player.level().isClientSide()) {
+				_player.displayClientMessage(Component.literal("Hi From Java").withStyle(net.minecraft.ChatFormatting.GOLD, net.minecraft.ChatFormatting.BOLD), false);
+			}
+		} else {
+			if (entity instanceof Player _player && !_player.level().isClientSide())
+				_player.displayClientMessage(Component.literal("crouch right now"), false);
+		}
+
+	}
+
+	private static int getEntityScore(String score, Entity entity) {
+		Scoreboard scoreboard = entity.level().getScoreboard();
+		Objective scoreboardObjective = scoreboard.getObjective(score);
+		if (scoreboardObjective != null)
+			return scoreboard.getOrCreatePlayerScore(ScoreHolder.forNameOnly(entity.getScoreboardName()), scoreboardObjective).get();
+		return 0;
+	}
+}
--- iko/ilovesoccer/src/main/java/net/mcreator/ilovesoccer/procedures/SpellbookRightclickedProcedure.java
+++ iko/ilovesoccer/src/main/java/net/mcreator/ilovesoccer/procedures/SpellbookRightclickedProcedure.java
@@ -1,8 +1,5 @@
 package net.mcreator.ilovesoccer.procedures;
 
-import net.minecraft.world.scores.Scoreboard;
-import net.minecraft.world.scores.ScoreHolder;
-import net.minecraft.world.scores.Objective;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.entity.player.Player;
@@ -46,26 +43,7 @@ public class SpellbookRightclickedProcedure {
 				}, _bpos);
 			}
 		} else {
-			spell = getEntityScore("current_spell", entity);
-			if (spell == 0) {
-				if (entity instanceof Player _player && !_player.level().isClientSide())
-					_player.displayClientMessage(Component.literal("Hi from MCreator"), false);
-			} else if (spell == 1) {
-				if (entity instanceof Player _player && !_player.level().isClientSide()) {
-					_player.displayClientMessage(Component.literal("Hi From Java").withStyle(net.minecraft.ChatFormatting.GOLD, net.minecraft.ChatFormatting.BOLD), false);
-				}
-			} else {
-				if (entity instanceof Player _player && !_player.level().isClientSide())
-					_player.displayClientMessage(Component.literal("crouch right now"), false);
-			}
+			CastProcedure(entity, x, y, z);
 		}
-	}
-
-	private static int getEntityScore(String score, Entity entity) {
-		Scoreboard scoreboard = entity.level().getScoreboard();
-		Objective scoreboardObjective = scoreboard.getObjective(score);
-		if (scoreboardObjective != null)
-			return scoreboard.getOrCreatePlayerScore(ScoreHolder.forNameOnly(entity.getScoreboardName()), scoreboardObjective).get();
-		return 0;
 	}
 }
